a1320153d2b43c864a4cfe070ca0bba3df349dbd
takoyaro <thetakoyaro@gmail.com>
2022-11-08 10:45:27 +0900

    feat: 🎸 prompts

--- src/cmd.ts
+++ src/cmd.ts
@@ -1,16 +1,3 @@
-
-export const CREATED_PULL_REQUESTS = [
-  'gh',
-  `pr',
-  'list',
-  '--author "@me"',
-  '-s all',
-  '--json title,url,updatedAt'
-];
-
-export const REQUESTED_REVIEW = [
-  `gh`, 
-  `pr list -S "user-review-requested:@me" -s all --json title,url,updatedAt`
-];
-
+export const CREATED_PULL_REQUESTS = `gh pr list -A "@me" -s all --json title,url,updatedAt`;
+export const REQUESTED_REVIEW = `gh pr list -S "user-review-requested:@me" -s all --json title,url,updatedAt`;
 
